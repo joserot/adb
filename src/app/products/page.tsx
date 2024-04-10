1e2eb081a97b3cf5bd8bdc66ac2c9cb8fcb642e6
Jose Rotchen <jose.rotchen14@gmail.com>
2024-04-10 17:05:29 -0300

    pagina de productos links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,37 +7,37 @@ const productsList = [
   {
     name: "Wierner Lab",
     image: "/img/products-new/3.jpeg",
-    url: "https://drive.google.com/file/d/1AJCiGVwpZRb1t7YgMwy0WMBTR1Pybfhj/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1HJ9VLJqARGVy7j98ZlJ0j7Ypsasx2WNj/view?usp=sharing",
   },
   {
     name: "Britania Lab",
     image: "/img/products-new/5.jpeg",
-    url: "https://drive.google.com/file/d/1wsEaHhcL_EydQ00TH9o6dG3xErZ3UDjp/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1vECn5MfZDt248mCQuWmyKTGFqJVStwW1/view?usp=sharing",
   },
-  {
-    name: "Tubos Britania",
-    image: "/img/products-new/2.jpeg",
-    url: "https://drive.google.com/file/d/1CS9hzMOKZlv6njMeGj6rZpOYED719Ddj/view?usp=drive_link",
-  },
+  // {
+  //   name: "Tubos Britania",
+  //   image: "/img/products-new/2.jpeg",
+  //   url: "https://drive.google.com/file/d/1CS9hzMOKZlv6njMeGj6rZpOYED719Ddj/view?usp=drive_link",
+  // },
   {
     name: "Biopack",
     image: "/img/products-new/4.jpeg",
-    url: "https://drive.google.com/file/d/1-H4-zvyixwX6AJPs8wgLyHzHdSH98KqU/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/165tO95h8fyOYwBLhEJCtil1SVxqSIS9G/view?usp=sharing",
   },
   {
     name: "Test rápidos",
     image: "/img/products-new/1.jpeg",
-    url: "https://drive.google.com/file/d/10umOiDcesbm8JvheAIKn0nBQ4tItMa-I/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/13CWhaz8oRUXnk7WYnjet766cW14S4zex/view?usp=sharing",
   },
   {
     name: "Everglass",
     image: "/img/products-new/7.jpeg",
-    url: "https://drive.google.com/file/d/13oXYsA704GaBcapRUDXSElRe94cCTsnE/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1LpUWtcind9OKOv7OIp4k9rzribcloUw7/view?usp=sharing",
   },
   {
     name: "Paralwall",
     image: "/img/products-new/8.jpeg",
-    url: "https://drive.google.com/file/d/1hYdu9q8z6ZbGP1Fi7cyLDjzmMgn2lMua/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1nxf2fIsKWii8Hf2FgA35FRExA_y3bxv5/view?usp=sharing",
   },
 ];
 
